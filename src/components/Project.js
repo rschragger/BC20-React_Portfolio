432f148048ae04f10ec13cc35d80924fba7d12fe
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-10-12 15:00:07 +1100

    updates and bugfixes

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,13 +8,19 @@ const Project = ({ projectsData }) => {
         {projectsData.map((item) =>
           <div className="col-sm-6 " key={item.id}>
             <div className="card m-1 p-2" >
+              <h4 className="card-title">{item.id}. {item.title}</h4>
+
               <div className="embed-responsive embed-responsive-2by1">
                 <img alt={item.title} className="card-img-top embed-responsive-item" src={item.imageUrl} />
               </div>
-              <h3 className="card-title">{item.id} {item.title}</h3>
-              <desc className="card-text">{item.description}</desc>
-              <depl className="card-text">{item.deployedLink}</depl>
-              <githb className="card-text">{item.githubLink}</githb>
+              <div className="desc card-text">{item.description}</div>
+
+              <div class="btn-group" role="group" aria-label="Reference Points">
+                <button className="depl btn btn-secondary p-1"><a href={item.deployedLink}>Application</a></button>
+                <button className="depl btn btn-secondary p-1"><a href={item.githubLink}>Repository</a></button>
+              </div>
+
+
             </div>
           </div>
         )}
